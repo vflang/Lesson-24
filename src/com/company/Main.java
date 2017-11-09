fe0414cee35b7adc9ab8a411b8db32bf650b13c0
Veronica Lang <TheSlug64@gmail.com>
2017-11-09 17:16:18 -0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,48 @@
 package com.company;
+import java.util.*;
+import java.io.*;
 /*
-Encrypted Data Project Goal:
-taking a word, generating an encryption key, and sending the
-encrypted data to a CLASS where it is stored.
-If user requests the word, the user may retrieve it by accessing the decryption key
-As an added bonus, if user 1 gives access to user 2, user 1 and user 2 can decipher each others word
-^^^how data can pass through classes and how only certain objects may be granted access
-
-Planning:
-- Use Caesar Cipher; use a method in a class for crypting and decripting; if user 1 gives access to 2 have
-another method that decripts the message for user 2
-- Objects would be users?
-- maybe have a method that allows user 1 to give access to user 2; maybe use a SOP that asks user 1 if they
-want to give access to user 2 --> decryption method and etc
-- check if all letters of the word that's supposed to be encrypted are letters not numbers
+Supposed to output
+Agnes, average = 76
+Bufford, average = 91
+Julie, average = 94
+Alice, average = 39
+Bobby, average = 93
+i hope we'll be able to see a solution because im not really sure what i'm doing
+same for the twip...
  */
 public class Main {
 
-    public static void main(String[] args) {
-	EncryptedData obj = new EncryptedData();
-	obj.encrypt();
-	obj.decrypt();
-	obj.access();
+    public static void main(String[] args) throws IOException {
+        Scanner a = new Scanner(new File("StudentScores.in.txt"));
+        int maxIndx = -1;
+        String text[] = new String[1000];
+        while (a.hasNext()) {
+            maxIndx++;
+            text[maxIndx] = a.nextLine();
+            a.close();
+        } //^^puts the text into an array
+
+        for (int j = 0; j <= maxIndx; j++) {
+            Scanner b = new Scanner(text[j]);
+            String names[] = new String[1000];
+            while (b.hasNext()) {
+                int maxIndx2 = -1;
+                maxIndx2++;
+                names[maxIndx2] = b.next();
+            }
+        }//^^puts the names into an array
+
+        for (int i = 0; i <= maxIndx; i++) {
+            Scanner c = new Scanner(text[i]);
+            Integer numbers[] = new Integer[1000];
+            int countNum = 0;
+            while (c.hasNext()) {
+                int maxIndx2 = -1;
+                maxIndx2++;
+                numbers[maxIndx2] = c.nextInt();
+                countNum++; //to find out how much we gotta divide the grades by
+            }
+        }//puts the numbers into an array
     }
 }
